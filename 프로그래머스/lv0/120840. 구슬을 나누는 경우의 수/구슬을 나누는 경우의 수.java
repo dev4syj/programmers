9958f0a7fff95c4438143a96c97c20d29bf6c987
dev4syj <129806812+dev4syj@users.noreply.github.com>
2023-07-19 20:12:03 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.03 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -6,6 +6,7 @@ class Solution {
         for (int i = 1; i <= share; i++) {
             // 재귀함수 쓰면 점점 커지다 어느 시점에서 overflow 발생 가능
             // i 늘리면서 바로 계산하기 -> 나눠주면서 크기 조절
+            // 약분될 값은 곱하지 않고 필요한 값만 계산
             result = result * (balls - i + 1) / i;
         }
         return (int) result;
