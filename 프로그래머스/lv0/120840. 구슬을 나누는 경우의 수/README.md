4a8dd888a9fe2571ebb84fa49a677bd4a6f0b097
dev4syj <129806812+dev4syj@users.noreply.github.com>
2023-07-19 20:06:43 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/README.md
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 구슬을 나누는 경우의 수 - 120840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840) 
 
 ### 성능 요약
 
-메모리: 76.9 MB, 시간: 0.04 ms
+메모리: 72.8 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -1,17 +1,15 @@
 import java.util.*;
 class Solution {
     public int solution(int balls, int share) {
-        // Calculate the minimum value of share and balls-share
         int minShare = Math.min(share, balls - share);
-
-        // Initialize the result to 1
-        long result = 1;
-
-        // Calculate the combination using the formula
+        long result = 1; // overflow 막기 위해 int 대신 사용
+        
+        // 경우의 수 -> 조합으로 계산
         for (int i = 1; i <= minShare; i++) {
+            // 재귀함수 쓰면 점점 커지다 어느 시점에서 overflow 발생 가능
+            // i 늘리면서 바로 계산하기 -> 나눠주면서 크기 조절
             result = result * (balls - i + 1) / i;
         }
-
         return (int) result;
     }
 }
