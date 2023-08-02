16cc9bf138fbf9c4735e9d81b4b2f193efccf964
dev4syj <129806812+dev4syj@users.noreply.github.com>
2023-08-02 14:06:51 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    
+    public int 유클리드호제법(int n, int m) {
+        if (n % m == 0) return m;
+        return 유클리드호제법(m, n % m);
+    }
+    
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        answer[0] = 유클리드호제법(n, m);
+        answer[1] = n * m / answer[0];
+        return answer;
+    }
+}
