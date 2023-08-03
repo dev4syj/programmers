aa12a5705d1dd8a19ec12398b4aa2740775ad0c5
dev4syj <129806812+dev4syj@users.noreply.github.com>
2023-08-03 20:13:01 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.01 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -3,22 +3,20 @@ class Solution {
         int len = arr1.length;
         String[] bi1 = new String[len];
         String[] bi2 = new String[len];
-        int max_len = Integer.MIN_VALUE; // 전체 지도 최대 크기
-        
+
         // 이진수로 바꾸기
         for (int i = 0; i < len; i++) {
             bi1[i] = Integer.toBinaryString(arr1[i]);
             bi2[i] = Integer.toBinaryString(arr2[i]);
-            max_len = Math.max(max_len, Math.max(bi1[i].length(), bi2[i].length()));
         }
 
         // 변환하기
         String[] code = new String[len];
         for (int i = 0; i < len; i++) {
-            bi1[i] = "0".repeat(max_len - bi1[i].length()) + bi1[i];
-            bi2[i] = "0".repeat(max_len - bi2[i].length()) + bi2[i];
+            bi1[i] = "0".repeat(n - bi1[i].length()) + bi1[i];
+            bi2[i] = "0".repeat(n - bi2[i].length()) + bi2[i];
             String tmp = "";
-            for (int j = 0; j < max_len; j++) {
+            for (int j = 0; j < n; j++) {
                 if (bi1[i].charAt(j) == '1' || bi2[i].charAt(j) == '1') tmp += "#";
                 else tmp += " ";
             }
