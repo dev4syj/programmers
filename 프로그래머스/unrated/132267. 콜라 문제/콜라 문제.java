51ee537ffa3ee07415b2d5efa24eb1a2c28cffd5
dev4syj <129806812+dev4syj@users.noreply.github.com>
2023-08-04 13:59:58 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -1,14 +1,11 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        if (n < a) return 0;
-        else {
-            int service = 0, rest = n;
-            while (rest >= a) {
-                service = rest / a * b;
-                answer += service;
-                rest = rest % a + service;
-            }
+        int service = 0, rest = n;
+        while (rest >= a) {
+            service = rest / a * b;
+            answer += service;
+            rest = rest % a + service;
         }
         return answer;
     }
